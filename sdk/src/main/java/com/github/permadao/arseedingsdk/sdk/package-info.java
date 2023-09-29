21ed06b237d55d6fd218b3ca8a9c801f83aa5b23
“shiwenwy” <wangyao99163@163.com>
2023-09-29 19:55:28 +0800

    dev init

---
+++ sdk/src/main/java/com/github/permadao/arseedingsdk/sdk/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author shiwen.wy
+ * @date 2023/9/29 19:37
+ */
+package main.java.com.github.permadao.arseedingsdk.sdk;
---
+++ util/src/main/java/com/github/permadao/arseedingsdk/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author shiwen.wy
+ * @date 2023/9/29 19:06
+ */
+package com.github.permadao.arseedingsdk.util;
