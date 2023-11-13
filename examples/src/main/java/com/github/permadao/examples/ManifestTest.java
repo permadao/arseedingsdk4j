e1daa3b23088efc58ea9db5f9a639010c6c196d1
0x567 <754162382@qq.com>
2023-11-14 07:38:09 +0800

    optimize code

--- examples/src/main/java/com/github/permadao/examples/ManifestTest.java
+++ examples/src/main/java/com/github/permadao/examples/ManifestTest.java
@@ -15,7 +15,7 @@ public class ManifestTest {
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         builder.build();
         Wallet ethereumWallet = EthereumWallet.loadEthereumWallet(
-                "1d8bdd0d2f1e73dffe1111111118325b7e195669541f76559760ef615a588be3");
+                "cwtech78c954d792251666305c59526a71cb6eb70052cfb09556432fcf01eff838b0");
         System.out.println(ethereumWallet.getAddress());
         ArSeedingService arSeedingService =
                 new HttpArSeedingService("https://seed-dev.everpay.io",
